f2c8bf38aac33a3d56d6be800213642aa04202d1
sidhanathdon <137877586+sidhanathdon@users.noreply.github.com>
2023-11-30 16:02:14 +0530

    Update Page.js

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,18 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { LocalThemedBox } from './LocalThemedBox';
 import { ThemeContext } from './ThemeProvider';
 
 const Page = () => {
-
+      const themeToggle = useContext(ThemeContext)
     return(
-        <div className={"container"} id="themed-page">
-            <p id="themed-text-container">
+        <div className={`container bg-${themeToggle.theme}`} id="themed-page">
+            <p id="themed-text-container" className={`txt-${themeToggle.theme}`}>
                 lorem ipsum dolor iterit n stuff
             </p>
-            <button className="btn" id="themed-button">Themed Button</button>
+            <button className="btn" id="themed-button">Sup</button>
             <LocalThemedBox />
         </div>
     )
 }
-
 export { Page }
